f9f853d6e5c47b92970aa1aba9eabd75fb3d7948
Rohanvp07 <49580063+Rohanvp07@users.noreply.github.com>
2021-03-08 08:43:25 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,62 @@
+from flask import Flask, render_template, request
+import requests
+import pickle
+import sklearn
+from sklearn.preprocessing import StandardScaler
+app = Flask(__name__)
+model = pickle.load(open('model.pkl', 'rb'))
+@app.route('/',methods=['GET'])
+def Home():
+    return render_template('index.html')
+
+
+standard_to = StandardScaler()
+@app.route("/predict", methods=['POST'])
+def predict():
+    Fuel_Type_Diesel=0
+    if request.method == 'POST':
+        Year = int(request.form['Year'])
+        Present_Price=float(request.form['Present_Price'])
+        Kms_Driven=int(request.form['Kms_Driven'])
+        Owner=int(request.form['Owner'])
+        Fuel_Type_Petrol=request.form['Fuel_Type_Petrol']
+        if(Fuel_Type_Petrol=='CNG'):
+                Fuel_Type_CNG=1
+                Fuel_Type_Diesel=0
+                Fuel_Type_Petrol=0
+        elif(Fuel_Type_Petrol=='Diesel'):
+                Fuel_Type_CNG=0
+                Fuel_Type_Diesel=1
+                Fuel_Type_Petrol=0
+        else:
+                Fuel_Type_CNG=0
+                Fuel_Type_Diesel=0
+                Fuel_Type_Petrol=1
+
+        Year=2020-Year
+        Seller_Type_Individual=request.form['Seller_Type_Individual']
+        if(Seller_Type_Individual=='Individual'):
+            Seller_Type_Individual=1
+            Seller_Type_Dealer=0
+        else:
+            Seller_Type_Dealer=1
+            Seller_Type_Individual=0	
+        Transmission_Mannual=request.form['Transmission_Mannual']
+        if(Transmission_Mannual=='Mannual'):
+            Transmission_Mannual=1
+            Transmission_Automatic=0
+        else:
+            Transmission_Automatic=1
+            Transmission_Mannual=0
+        prediction=model.predict([[Present_Price,Kms_Driven,Owner,Year,Fuel_Type_CNG,Fuel_Type_Diesel,Fuel_Type_Petrol,Seller_Type_Dealer,Seller_Type_Individual,Transmission_Automatic,Transmission_Mannual]])
+        output=round(prediction[0],2)
+        if output<0:
+            return render_template('index.html',prediction_texts="Sorry you cannot sell this car")
+        else:
+            return render_template('index.html',prediction_text="You Can Sell The Car at {}".format(output))
+    else:
+        return render_template('index.html')
+
+if __name__=="__main__":
+    app.run(debug=True)
+
