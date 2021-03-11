0b5233459820b07f86c64f5e5a62e2ee716ad120
Rohan Patil <rohanvp009@gmail.com>
2021-03-11 20:12:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ def predict():
         if output<0:
             return render_template('index.html',prediction_texts="Sorry you cannot sell this car")
         else:
-            return render_template('index.html',prediction_text="You Can Sell The Car at {}".format(output))
+            return render_template('index.html',prediction_text="You Can Sell The Car at {} lakhs".format(output))
     else:
         return render_template('index.html')
 
