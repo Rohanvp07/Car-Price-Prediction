62b6b61c98b091f669a05411c0b7ad843307d56a
Rohan Patil <rohanvp009@gmail.com>
2021-03-16 05:29:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,13 @@
 from flask import Flask, render_template, request
 import requests
 import pickle
-import sklearn
-from sklearn.preprocessing import StandardScaler
+
 app = Flask(__name__)
 model = pickle.load(open('model.pkl', 'rb'))
 @app.route('/',methods=['GET'])
 def Home():
     return render_template('index.html')
 
-
-standard_to = StandardScaler()
 @app.route("/predict", methods=['POST'])
 def predict():
     Fuel_Type_Diesel=0
@@ -53,7 +50,7 @@ def predict():
         if output<0:
             return render_template('index.html',prediction_texts="Sorry you cannot sell this car")
         else:
-            return render_template('index.html',prediction_text="You Can Sell The Car at {} lakhs".format(output))
+            return render_template('index.html',prediction_text="You Can Sell Your Car at {} Lakhs".format(output))
     else:
         return render_template('index.html')
 
