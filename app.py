7a3788371945be645ad2fe043f2349ebcc58ef88
Rohan Patil <rohanvp009@gmail.com>
2021-09-28 17:34:31 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
 from flask import Flask, render_template, request
 import requests
 import pickle
-
+import sklearn
+from sklearn.preprocessing import StandardScaler
 app = Flask(__name__)
-
 model = pickle.load(open('model.pkl', 'rb'))
-
 @app.route('/',methods=['GET'])
 def Home():
-    return render_template('input.html')
+    return render_template('index.html')
 
-@app.route('/predict', methods=['POST'])
+
+standard_to = StandardScaler()
+@app.route("/predict", methods=['POST'])
 def predict():
     Fuel_Type_Diesel=0
     if request.method == 'POST':
@@ -50,7 +51,7 @@ def predict():
         prediction=model.predict([[Present_Price,Kms_Driven,Owner,Year,Fuel_Type_CNG,Fuel_Type_Diesel,Fuel_Type_Petrol,Seller_Type_Dealer,Seller_Type_Individual,Transmission_Automatic,Transmission_Mannual]])
         output=round(prediction[0],2)
         if output<0:
-            return render_template('index.html',prediction_text="Sorry you cannot sell this car")
+            return render_template('index.html',prediction_texts="Sorry you cannot sell this car")
         else:
             return render_template('index.html',prediction_text="You Can Sell Your Car at {} Lakhs".format(output))
     else:
